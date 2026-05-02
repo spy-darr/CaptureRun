64a74ec85456e634e420940f36c983500e8bf0e3
spy-darr <100486516+spy-darr@users.noreply.github.com>
2026-05-02 15:32:52 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,10 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyDj1IhTqlHxhjniN1NeGIyjaE1QuxkEk1o",
+  authDomain: "capturerun-27ebc.firebaseapp.com",
+  databaseURL: "https://capturerun-27ebc-default-rtdb.firebaseio.com",
+  projectId: "capturerun-27ebc",
+  storageBucket: "capturerun-27ebc.firebasestorage.app",
+  messagingSenderId: "690859670858",
+  appId: "1:690859670858:web:e62118f0bfab8f0c7c55b3",
+  measurementId: "G-391DD6HVWL"
 };
 
 const app = initializeApp(firebaseConfig);
